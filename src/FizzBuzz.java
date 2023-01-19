0716325ab8d681cb99c4e41d3ae96b2139c44654
N.Tomohiro <NagahataTomohiro@users.noreply.github.com>
2023-01-19 19:29:51 +0900

    課題提出

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,7 +1,8 @@
 public class FizzBuzz {
 	public static void main(String[] args) {
 		for (int number = 1; number <= 100; number++) {
-
+			
+			
 			if (number % 3 == 0 && number % 5 == 0) {
 				System.out.println("FizzBuzz");
 			} else if (number % 3 == 0) {
